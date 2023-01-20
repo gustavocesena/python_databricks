aff7384f3273eda9c3cda1007269d12c48047c6d
gcesena <gcesena@insulet.com>
2023-01-19 23:20:18 -0800

    Create bg events unit of work

---
+++ unit_of_work/bg_events_uow.py
@@ -0,0 +1,6 @@
+from repositories.blob.insights_factory_repository import InsightsFactoryRepository
+
+
+class BGEventsUOW:
+    def __init__(self, insights_factory_repository: InsightsFactoryRepository):
+        self.insights_factory_repository = insights_factory_repository()
